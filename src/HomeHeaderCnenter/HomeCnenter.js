500e72b495e2b5584fe23f56914befca6c9c8f55
murtazahassani69 <murtazahassani105@gmail.com>
2021-01-05 18:55:27 +0200

    Header section

--- src/HomeHeaderCnenter/HomeCnenter.js
+++ src/HomeHeaderCnenter/HomeCnenter.js
@@ -12,22 +12,24 @@ export default function HomeCnenter() {
     <div className="homecenter">
       <div>
         <div styles="border-radius: 0px" className="img_center">
-          <h1>
-            Welcome to
-            <br />
-            Amnick!
-          </h1>
-          <br />
-          <h4>
-            We are a combination of City <br />
-            Leaders/Mayors from around the world.
-          </h4>
-          <div>
-            <p>
-              Whatever your life’s goals, <br /> our financial professionals can help
-              design a strategy tailored to your specific needs. 
-              <br />
-            </p>
+        <div className="tit_welcome">
+        <h1>
+        Welcome to
+        
+        Amnick!
+        </h1>
+        <br />
+        <h4>
+        We are a combination of City <br />
+        Leaders/Mayors from around the world.
+        </h4>
+        <div>
+        <p>
+        Whatever your life’s goals, <br /> our financial professionals can help
+        design a strategy tailored to your specific needs. 
+        <br />
+        </p>
+        </div>
             <button className="button1">
 Explore</button>
       </div>
