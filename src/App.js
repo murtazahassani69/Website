b24953cf1e6a260798811b8cb839113c8673c26e
murtazahassani69 <murtazahassani105@gmail.com>
2021-01-08 00:21:47 +0200

    New changes in the background

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
-import './App.css';
-import {BrowserRouter as Router, Route} from 'react-router-dom';
-import HomeCnenter from './HomeHeaderCnenter/HomeCnenter';
+import "./App.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import HomeCnenter from "./HomeHeaderCnenter/HomeCnenter";
+import particlesConfig from "./config/particlesConfig";
+import Particles from "react-particles-js";
 
+//   {/* Mutaza and Presefoni Part */}
 function App() {
   return (
     <Router>
-    {/* Mutaza and Presefoni Part */}
-      <Route exact path="/" component={HomeCnenter} />
-  </Router>
+      <div className="App">
+        <div style={{ position: "absolute" }}>
+          <Particles className="Particless"  height="100vh" marginTop="143px" width="100vw" justifyContent="center" params={particlesConfig} />
+        </div>
+        <header className="App-header">
+          <Route exact path="/" component={HomeCnenter} />
+        </header>
+      </div>
+    </Router>
   );
 }
 
--- src/HomeHeaderCnenter/HomeCnenter.js
+++ src/HomeHeaderCnenter/HomeCnenter.js
@@ -1,44 +1,27 @@
 import React from "react";
-import { makeStyles, createStyles, Theme } from "@material-ui/core/styles";
-import Paper from "@material-ui/core/Paper";
-import Grid from "@material-ui/core/Grid";
 import "./HomeCnenter.css";
-import Button from '@material-ui/core/Button';
-
 
 export default function HomeCnenter() {
-
   return (
     <div className="homecenter">
       <div>
         <div styles="border-radius: 0px" className="img_center">
-        <div className="tit_welcome">
-        <h1>
-        Welcome to
-        
-        Amnick!
-        </h1>
-        <br />
-        <h4>
-        We are a combination of City <br />
-        Leaders/Mayors from around the world.
-        </h4>
-        <div>
-        <p>
-        Whatever your life’s goals, <br /> our financial professionals can help
-        design a strategy tailored to your specific needs. 
-        <br />
-        </p>
+          <div className="tit_welcome">
+            <h1>Welcome to Amnick!</h1>
+            <h4>
+              We are a combination of City <br />
+              Leaders/Mayors from around the world.
+            </h4>
+            <div>
+              <p>
+                Whatever your life’s goals, <br /> our financial professionals
+                can help design a strategy tailored to your specific needs.
+                <br />
+              </p>
+            </div>
+            <button className="button1">Explore</button>
+          </div>
         </div>
-            <button className="button1">
-Explore</button>
-      </div>
-        </div>  
-        <br />
-        <br />
-        <br />
-        <br />
-
       </div>
     </div>
   );
