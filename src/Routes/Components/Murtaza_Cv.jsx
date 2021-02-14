af46e74c4f35db7948cf3b8054861bba14cf12a6
murtazahassani69 <murtazahassani105@gmail.com>
2021-02-14 22:03:00 +0200

    just to get the lastest changes

---
+++ src/Routes/Components/Murtaza_Cv.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Home_Cv from "./CV-S/Murtaza_CV/Home_Cv";
+
+function Murtaza_Cv() {
+  return (
+    <div>
+      <Home_Cv />
+    </div>
+  );
+}
+
+export default Murtaza_Cv;
