4dc2951003a7eb3c3978e4c9d0f16a1c2b03ef8d
murtazahassani69 <murtazahassani105@gmail.com>
2021-02-16 21:18:00 +0200

    Murtaza cv part aded

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Home from "./Routes/Components/Home";
 import Restaurant from "./Routes/Components/Restaurant";
 import Store from "./Routes/Components/StoreOnLine";
 import TempJ1 from "./Routes/Components/TempJa1";
+import Murtaza_Tamp from "./Routes/Components/Murtaza_Tamp";
 import Error from "./Routes/Components/Error";// Notice the keyword "exact" so react just renders the default page when nothing but the '/' is on it.
 
 // Swith Component renders the first match and ignore the rest.//   {/* Mutaza and Presefoni Part */}
@@ -18,6 +19,7 @@ function App() {
           <Route path='/store' exact component={Store} /> 
           <Route path='/templateMa1' exact component={Store} /> 
           <Route path='/templateJ1' exact component={TempJ1} /> 
+          <Route path='/Murtaza_Tamp' exact component={Murtaza_Tamp} /> 
           <Route path='/templateB1' exact component={Store} /> 
           <Route path='/templateY1' exact component={Store} /> 
           <Route path='/templateMu1' exact component={Store} /> 
---
+++ src/Routes/Components/Murtaza_Tamp.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import CvTampMortazaHome_Cv from "./Demo/CVTemplates/MurtazaTemplate/CvTampMortazaHome_Cv";
+
+function Murtaza_Tamp() {
+    return (
+        <div>
+            <CvTampMortazaHome_Cv />
+        </div>
+    )
+}
+
+export default Murtaza_Tamp
