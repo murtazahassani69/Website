996103579c2799bf355a57a2a1652ce68180359b
murtazahassani69 <murtazahassani105@gmail.com>
2021-03-14 20:34:50 +0200

    Murtaza CV templates has been updated!

---
+++ src/Routes/Components/Demo/CVTemplates/SecondMurtazaTemplate/SecondCvTampMortazaHome_Cv.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Sidebar from "./Sidebar";
+import Home from "./Home";
+import "../../../../css/SecondMurtazaCVTemp.css";
+function SecondCvTampMortazaHome_Cv() {
+  return (
+    <div className="Second_MurtazaCV_Template">
+      <Home />
+      <Sidebar />
+    </div>
+  );
+}
+
+export default SecondCvTampMortazaHome_Cv;
---
+++ src/Routes/Components/SecondMurtaza_Tamp.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import SecondCvTampMortazaHome_Cv from '../Components/Demo/CVTemplates/SecondMurtazaTemplate/SecondCvTampMortazaHome_Cv'
+function SecondMurtaza_Tamp() {
+    return (
+        <div>
+            <SecondCvTampMortazaHome_Cv />
+        </div>
+    )
+}
+
+export default SecondMurtaza_Tamp
+    
