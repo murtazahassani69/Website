173a2fe2289fcbe8c44d089c6da08f0d07e70d30
murtazahassani69 <murtazahassani105@gmail.com>
2021-03-29 18:55:29 +0300

    resolving the conflicts

--- src/App.js
+++ src/App.js
@@ -14,22 +14,26 @@ import MurtazaTampReview from './Routes/Components/Demo/CVTemplates/MurtazaTempl
 function App() {
   return (
     <Router>
-      <div className="App">
-        <Switch>
-          <Route path='/' exact component={Home} />   
-          <Route path='/restaurant' exact component={Restaurant} /> 
-          <Route path='/store' exact component={Store} /> 
-          <Route path='/templateMa1' exact component={Store} /> 
-          <Route path='/templateJ1' exact component={TempJ1} /> 
-          <Route path='/Murtaza_Tamp' exact component={Murtaza_Tamp} /> 
-          <Route path='/SecondMurtaza_Tamp' exact component={SecondMurtaza_Tamp} /> 
-          <Route path='/MurtazaTampReview' exact component={MurtazaTampReview} /> 
-          <Route path='/templateB1' exact component={Store} /> 
-          <Route path='/templateY1' exact component={Store} /> 
-          <Route path='/templateMu1' exact component={Store} /> 
-          <Route path='*' component={Error}/>
-        </Switch>
-      </div>
+    <div className="App"> 
+      <Switch>
+        <Route path='/' exact component={Home} />   
+        <Route path='/restaurant' exact component={Restaurant} /> 
+        <Route path='/store' exact component={Store} /> 
+        <Route path='/templates' exact component={Templates} /> 
+        <Route path='/templateMa1' exact component={TempMa1} /> 
+        <Route path='/templateJ1' exact component={TempJ1} /> 
+        <Route path='/templateJ2' exact component={TempJ2} /> 
+        <Route path='/templateJ3' exact component={TempJ3} /> 
+        <Route path='/templateB1' exact component={TempB1} /> 
+        <Route path='/templateB2' exact component={TempB2} /> 
+        <Route path='/templateY1' exact component={TempY1} /> 
+        <Route path='/templateY2' exact component={TempY2} /> 
+        <Route path='/templateY3' exact component={TempY3} /> 
+        <Route path='/templateMu1' exact component={TempMu1} /> 
+        <Route path='/templateMu2' exact component={TempMu2} /> 
+        <Route path='*' component={Error}/>
+      </Switch>
+    </div>
     </Router>
   );
 }
