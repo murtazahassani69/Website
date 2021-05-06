779d77a288bb6bd9e79858d5c4bf1845225e27b3
Murtaza Hassani <65913750+murtazahassani69@users.noreply.github.com>
2021-05-06 14:09:22 +0300

    select language added

--- src/Routes/Components/Home/Header.js
+++ src/Routes/Components/Home/Header.js
@@ -1,23 +1,24 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
-import Particles from 'react-particles-js';
-import particlesConfig from '../../../config/particlesConfig';
-import '../../css/HomeHeader.css';
+import React from "react";
+import { NavLink } from "react-router-dom";
+import Particles from "react-particles-js";
+import particlesConfig from "../../../config/particlesConfig";
+import "../../css/HomeHeader.css";
+import { Text } from "./containers/Language";
 
 function Header() {
   return (
-    <div className='header-container '>
-      <Particles className='header-particles' params={particlesConfig} />
+    <div className="header-container ">
+      <Particles className="header-particles" params={particlesConfig} />
 
-      <div className='header-background-img'>
-        <div className='header-title'>
-          <div className='header-text'>
-            Let's get started together in the world of buisness.
+      <div className="header-background-img">
+        <div className="header-title">
+          <div className="header-text">
+            <Text tid="Welcome_message" />{" "}
           </div>
 
-          <div className='header-button'>
-            <NavLink to='/' activeClassName='header-active-title-btn'>
-              Let's go
+          <div className="header-button">
+            <NavLink to="/" activeClassName="header-active-title-btn">
+              <Text tid="Explore_message" />{" "}
             </NavLink>
           </div>
         </div>
--- src/Routes/Components/Home/Menu.js
+++ src/Routes/Components/Home/Menu.js
@@ -1,9 +1,14 @@
-import React, { useState } from 'react';
-import { NavLink } from 'react-router-dom';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBars, faTimes } from '@fortawesome/free-solid-svg-icons';
-import '../../css/HomeMenu.css';
-import logo_img from '../../imgs/footer_logo.png';
+import React, { useState, useContext } from "react";
+import { NavLink } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars, faTimes } from "@fortawesome/free-solid-svg-icons";
+import "../../css/HomeMenu.css";
+import logo_img from "../../imgs/footer_logo.png";
+import greek_logo from "../../imgs/greek_logo.jpg";
+import en_flag from "../../imgs/en_flag.png";
+import { LanguageContext } from "./containers/Language";
+
+import { languageOptions } from "./languages";
 
 function Menu() {
   const [toggle, setToggle] = useState(false);
@@ -13,26 +18,28 @@ function Menu() {
   console.log(toggle);
   const navBarOpts = [
     {
-      name: 'Home',
-      link: '/',
+      name: "Home",
+      link: "/",
     },
     {
-      name: 'About us',
-      link: '/',
+      name: "About us",
+      link: "/store",
     },
     {
-      name: 'Services',
-      link: '/',
+      name: "Services",
+      link: "/",
     },
     {
-      name: 'Our Work',
-      link: '/restaurant',
+      name: "Our Work",
+      link: "/restaurant",
     },
   ];
 
+  const { userLanguage, userLanguageChange } = useContext(LanguageContext);
+  const handleLanguageChange = (e) => userLanguageChange(e.target.value);
   const mapNavOpts = navBarOpts.map((el) => (
     <div key={el.name} onClick={() => setToggle(false)}>
-      <NavLink to={el.link} className='menu__navbar-active'>
+      <NavLink to={el.link} className="menu__navbar-active">
         {el.name}
       </NavLink>
     </div>
@@ -40,12 +47,27 @@ function Menu() {
 
   return (
     <>
-      <div className='menu_navbar-container'>
-        <img src={logo_img} alt='eskinous logo' className='menu__navbar-logo' />
-        <div className='menu__navbar-opts-container'>{mapNavOpts}</div>
+      <div className="menu_navbar-container">
+        <img src={logo_img} alt="eskinous logo" className="menu__navbar-logo" />
+        <div className="menu__navbar-opts-container">{mapNavOpts}</div>
+
+        <div className="dropdown_flag">
+          <select
+            className="dropdown-content_flag"
+            onChange={handleLanguageChange}
+            value={userLanguage}
+          >
+            {Object.entries(languageOptions).map(([id, name]) => (
+              <option key={id} value={id}>
+                {name}
+              </option>
+            ))}
+          </select>
+        </div>
+
         <FontAwesomeIcon
           icon={toggle ? faTimes : faBars}
-          className='menu__navbar-burger-menu'
+          className="menu__navbar-burger-menu"
           onClick={handleClick}
         />
       </div>
@@ -53,11 +75,11 @@ function Menu() {
       <div
         className={
           toggle
-            ? 'menu-dropdown-container'
-            : 'menu-dropdown-container-inactive'
+            ? "menu-dropdown-container"
+            : "menu-dropdown-container-inactive"
         }
       >
-        <div className='menu-dropdown-opts-cont'>{mapNavOpts}</div>
+        <div className="menu-dropdown-opts-cont">{mapNavOpts}</div>
       </div>
     </>
   );
